97dbedaf297fbb0392041cf342b0e66b998a2a5e
zy <zy318@qq.com>
2020-08-14 15:36:43 +0800

    test

--- src/main/java/com/lanxin/config/ShiroConfig.java
+++ src/main/java/com/lanxin/config/ShiroConfig.java
@@ -14,7 +14,6 @@ import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreato
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import java.util.HashMap;
 import java.util.Map;
 
